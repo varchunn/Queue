1bed16c3ce2b2d4ecf412ff76aca2edb849e867f
varchunn <varvaram186@gmail.com>
2024-11-04 00:32:51 +0200

    Initial Commit

---
+++ Queue/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Queue
+//
+//  Created by Варя Мінєнкова on 04.11.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
